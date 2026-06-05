abfc3c9a7cacfcceec0572b994b87fa72db8a864
Michael Rozentsvayg <rozentsvayg@gmail.com>
2026-06-05 11:02:28 +0300

    Update bronze.usp_LoadOperativeBronzeTables.sql

--- scripts/bronze.usp_LoadOperativeBronzeTables.sql
+++ scripts/bronze.usp_LoadOperativeBronzeTables.sql
@@ -1,1 +1,42 @@
+CREATE PROCEDURE [bronze].[usp_LoadOperativeBronzeTables]
+    
+    
+    @SourceServer SYSNAME, -- Will be needed for SSIS Foreach Loop Container to retrieve data from all POS Servers
+    @DateFrom DATE -- TODAY
+AS
+BEGIN
+    SET NOCOUNT ON;
+    
+    DECLARE @sql NVARCHAR(MAX); -- Dynamic SQL will be used
+    /* All source tables in all POS Servers belong to CashDB51 database, schema dbo */
+  
+    -- 1. CM - info about payments
+    SET @sql = N'
+    INSERT INTO bronze.oper_pos_cm_payments
+    SELECT
+      ,[UNIQ]
+      ,[SCODE]
+      ,[CHECKNUM]
+      ,[CASHCODE]
+      ,[SHIFT]
+      ,[FSHIFT]
+      ,[FSERIAL]
+      ,[DATE]
+      ,[TIME]
+      ,[OPCODE]
+      ,[VALCODE]
+      ,[NRATE]
+      ,[VSUM]
+      ,[SUMB]
+      ,[SUMN]
+      ,[SUME]
+      ,[DOPDATA]
+      ,[POSITION]
+    FROM 
+        [' + @SourceServer + '].[CashDB51].[dbo].[CM]
+    WHERE 
+        [DATE] = GETDATE()';
 
+  
+END
+GO
